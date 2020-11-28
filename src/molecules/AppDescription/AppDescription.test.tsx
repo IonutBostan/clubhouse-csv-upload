bbb7e7a2c9f25cce64d1a5a8bba6e581541a54eb
Ionut Bostan <ionut.bostan@smartprotection.com>
2020-11-28 23:08:46 +0100

    Add Remove API Token button

--- src/molecules/AppDescription/AppDescription.test.tsx
+++ src/molecules/AppDescription/AppDescription.test.tsx
@@ -1,18 +1,46 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, act, fireEvent } from "@testing-library/react";
 
 import { AppDescription } from "./AppDescription";
+import { ServiceContext } from "store/ServiceContext/ServiceContext";
 
 jest.mock("antd", () => {
+  const Button = ({ onClick }: any) => (
+    <button onClick={onClick}>button</button>
+  );
   const Typography = ({ children }: any) => <div>{children}</div>;
   Typography.Title = ({ children }: any) => <div>{children}</div>;
   Typography.Paragraph = ({ children }: any) => <div>{children}</div>;
-  return { Typography };
+  return { Typography, Button };
 });
 
+const mockSetClubhouseApiToken = jest.fn();
+
 describe("AppDescription component", () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
   it("renders correctly", () => {
     const renderInstance = render(<AppDescription />);
+    const { getByText } = renderInstance;
+    act(() => {
+      fireEvent.click(getByText(/button/i));
+    });
     expect(renderInstance.asFragment().firstChild).toBeTruthy();
+  });
+  it("calls the setClubhouseApiToken with an empty string when Remove API token is clicked", () => {
+    const serviceContextProviderValue = {
+      setClubhouseApiToken: mockSetClubhouseApiToken,
+    } as any;
+    const { getByText } = render(
+      <ServiceContext.Provider value={serviceContextProviderValue}>
+        <AppDescription />
+      </ServiceContext.Provider>
+    );
+    act(() => {
+      fireEvent.click(getByText(/button/i));
+    });
+    expect(mockSetClubhouseApiToken).toHaveBeenCalledTimes(1);
+    expect(mockSetClubhouseApiToken).toHaveBeenCalledWith("");
   });
 });
--- src/molecules/AppDescription/AppDescription.tsx
+++ src/molecules/AppDescription/AppDescription.tsx
@@ -1,13 +1,29 @@
-import { Typography } from "antd";
-import React from "react";
+import styled from "@emotion/styled";
+import { Button, Typography } from "antd";
+import React, { useContext } from "react";
+import { ServiceContext } from "store/ServiceContext/ServiceContext";
 const { Title, Paragraph } = Typography;
 
-export const AppDescription = () => (
-  <Typography>
-    <Title>Clubhouse CSV Importer</Title>
-    <Paragraph>
-      To import a csv file to clubhouse you need to select a milestone, select
-      an epic and after that upload a csv file with the stories
-    </Paragraph>
-  </Typography>
-);
+export const AppDescription = () => {
+  const { setClubhouseApiToken } = useContext(ServiceContext);
+  return (
+    <Typography>
+      <TitleWrapper>
+        <Title>Clubhouse CSV Importer</Title>
+        <Button onClick={() => setClubhouseApiToken("")} type="ghost">
+          Remove API Token
+        </Button>
+      </TitleWrapper>
+      <Paragraph>
+        To import a csv file to clubhouse you need to select a milestone, select
+        an epic and after that upload a csv file with the stories
+      </Paragraph>
+    </Typography>
+  );
+};
+
+const TitleWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
